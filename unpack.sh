93bb5b36c484c5c34c48c3d3265f5a279e9870c6
Willem de Groot <gwillem@gmail.com>
2015-08-21 11:54:12 +0200

    Update paths in unpack script

--- unpack.sh
+++ unpack.sh
@@ -1,1 +1,7 @@
- for x in *.gz; do base=$(basename $x .tar.gz); tar xf $x && mv magento $base; done
+cd sources
+for x in *.gz; do
+	base=$(basename $x .tar.gz);
+	target=../releases/$base
+	test -e $target || (tar xf $x && mv magento $target);
+done
+cd ..
