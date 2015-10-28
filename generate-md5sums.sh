7323005ee7eb8ed3c1cb324c1222b49bc74dddbc
Willem de Groot <gwillem@gmail.com>
2015-10-28 16:16:32 +0100

    New 1.9.2 checksums

--- generate-md5sums.sh
+++ generate-md5sums.sh
@@ -2,8 +2,10 @@
 for dir in releases/*;
 do
 	base=$(basename $dir)
-	echo Processing $base
+	md5file=$(echo $base | sed -e 's/magento-/magento-CE-/')
+	echo Processing $base to $md5file
+
 	cd releases/$base
-	find {js,media,skin} -type f -exec md5sum "{}" \; > ../../md5sums/$base
+	find {js,media,skin} -type f -exec md5sum "{}" \; > ../../md5sums/$md5file
 	cd ../..
 done
